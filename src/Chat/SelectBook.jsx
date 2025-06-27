7cc6bd641d14831921b4574175c6e9cc56e456ef
Juan Cantor <juancantor.all@gmail.com>
2025-06-27 01:22:10 -0500

    Book list in Ask module now fetchs existing books that have embeddings

--- src/Chat/SelectBook.jsx
+++ src/Chat/SelectBook.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Markdown from 'react-markdown'
-import { getPdf } from '../Api/Api.jsx'
+import { getPdf, useLoadBooks } from '../Api/Api.jsx'
 
 function SelectBook({setBook}) {
     const books = ["iama4.pdf", "TheEmperorsNewMind.pdf", "TheRoadToReality.pdf"]
+    const {
+        isSuccess: isSuccessLoadBooks,
+        data: dataLoadBooks,
+    } = useLoadBooks()
+
     const handleChange = async (e) => {
         if (e.target.value === ""){
             return
@@ -15,10 +20,15 @@ function SelectBook({setBook}) {
             <div className="card">
                 <select onChange={handleChange}>
                     <option value="" >..Select a book</option>
-                    {books.map((book, index)=>(
-                            <option value={book} key={index}>{book}</option>
-                        )
-                    )}
+                    {isSuccessLoadBooks &&
+                        dataLoadBooks.map((book, index)=>{
+                            if (book.embeddings){
+                                return (<option value={book.book} key={index}>{book.book}</option>)
+                            } else {
+                                return null
+                            }
+                        })
+                    }
                 </select>
             </div>
         )
